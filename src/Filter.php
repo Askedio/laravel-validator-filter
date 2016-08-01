12a85219654389c9de534e4d69f92a2dff5f4d24
William <gcphost@gmail.com>
2016-07-31 21:29:00 -0700

    fancy

--- src/Filter.php
+++ src/Filter.php
@@ -4,22 +4,77 @@ namespace Askedio\LaravelValidatorFilter;
 
 class Filter
 {
+    protected $attribute;
+
+    protected $value;
+
+    protected $parameters;
+
+    protected $validator;
+
+    protected $functional;
 
     public function __construct($attribute, $value, $parameters, $validator)
     {
+        $this->attribute = $attribute;
+        $this->value = $value;
+        $this->parameters = $parameters;
+        $this->validator = $validator;
 
-      $data = [$attribute => $value];
+        $this->setData();
 
-      app('sanitizer')->sanitize([$attribute => $parameters], $data);
+        $this->run();
+    }
 
-      $replace = array_merge(array_dot($validator->getData()), [$attribute => $data[$attribute]]);
-      
-      request()->replace($replace);
+    protected function run()
+    {
+        $this->replace($this->getData());
+    }
 
-      $validator->setData($replace);
+    protected function getData()
+    {
+        return $this->data;
+    }
 
-      return true;
+    protected function setData()
+    {
+        if ($this->paramsContainAFunction()) {
+            $this->data = [$this->attribute => $this->getValueFromFunction($this->value)];
+            return;
+        }
 
+        $this->data = $this->replaceWithSanitizer();
+    }
 
+    protected function replaceWithSanitizer()
+    {
+        $data = [$this->attribute => $this->value];
+
+        app('sanitizer')->sanitize([$this->attribute => $this->parameters], $data);
+
+        return $data;
+    }
+
+    protected function paramsContainAFunction()
+    {
+        preg_match('/(.*)\[(.*)\]/s', implode($this->parameters), $this->functional);
+
+        return count($this->functional) == 3 && function_exists($this->functional[1]);
+    }
+
+    private function getValueFromFunction($value)
+    {
+        return call_user_func_array($this->functional[1], array_map(function ($string) use ($value) {
+            return strtr($string, ['{$value}' => $value]);
+        }, explode(';', $this->functional[2])));
+    }
+
+    private function replace($data)
+    {
+        $replace = array_merge(array_dot($this->validator->getData()), [$this->attribute => $data[$this->attribute]]);
+
+        request()->replace($replace);
+
+        $this->validator->setData($replace);
     }
 }
--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -6,15 +6,31 @@ class FilterTest extends BaseTestCase
 {
     public function testThatFilterCanUseNl2Br()
     {
+        $string = 'Hello ' . PHP_EOL . ' World';
+
         $validator = app('validator')->make([
-          'title' => 'Hello ' . PHP_EOL . ' World'
+          'string' => $string,
         ], [
-          'title' => 'filter:nl2br',
+          'string' => 'filter:nl2br',
         ]);
 
-        $validator->fails();
+        $validator->passes();
 
-        $this->assertEquals($validator->getData()['title'], nl2br('Hello ' . PHP_EOL . ' World'));
+        $this->assertEquals($validator->getData()['string'], nl2br($string));
+    }
 
+    public function testThatFilterCanUseStripTagsButKeepBr()
+    {
+        $string = 'Hello <br> World<p></p>';
+
+        $validator = app('validator')->make([
+          'string' => $string,
+        ], [
+          'string' => 'filter:strip_tags[{$value}; "<br>"]',
+        ]);
+
+        $validator->passes();
+
+        $this->assertEquals($validator->getData()['string'], strip_tags($string, '<br>'));
     }
 }
